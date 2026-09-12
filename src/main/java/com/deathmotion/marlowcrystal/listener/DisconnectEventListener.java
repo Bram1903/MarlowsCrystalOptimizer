9a43fe2416bc7120d0c20b61d271083821b822e1
Bram <bram@bramdekker.com>
2026-09-12 13:13:11 +0200

    refactor: track opt-out per connection instead of per server

--- src/main/java/com/deathmotion/marlowcrystal/listener/DisconnectEventListener.java
+++ src/main/java/com/deathmotion/marlowcrystal/listener/DisconnectEventListener.java
@@ -1,7 +1,7 @@
 package com.deathmotion.marlowcrystal.listener;
 
 import com.deathmotion.marlowcrystal.MarlowCrystal;
-import com.deathmotion.marlowcrystal.cache.OptOutCache;
+import com.deathmotion.marlowcrystal.state.OptOutState;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientPacketListener;
@@ -9,14 +9,14 @@ import org.jetbrains.annotations.NotNull;
 
 public final class DisconnectEventListener implements ClientPlayConnectionEvents.Disconnect {
 
-    private final OptOutCache optOutCache;
+    private final OptOutState optOutState;
 
     public DisconnectEventListener() {
-        optOutCache = MarlowCrystal.getInstance().getOptOutCache();
+        optOutState = MarlowCrystal.getInstance().getOptOutState();
     }
 
     @Override
     public void onPlayDisconnect(@NotNull ClientPacketListener handler, @NotNull Minecraft client) {
-        optOutCache.clearCurrentSession();
+        optOutState.reset();
     }
 }
